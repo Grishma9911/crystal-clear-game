4a2bd76fc4ce8db2943a9c8f40a8305fdc6ca2bf
Grishma Patel <patelgrishma9911@gmail.com>
2019-07-17 19:37:10 -0400

    crystal game

--- java.js
+++ java.js
@@ -1,6 +1,88 @@
 var usersChoice = "";
-var me = document.getElementById("1");
-me.addEventListener("click", function(){
-    usersChoice = "1";
-    console.log(this)
+$("#one").on("click", function(){
+    usersChoice = "one";
 });
+
+var randomNumber = Math.floor(Math.random() * 5)
+$("#one").on("click", function() {
+    var randomNumber = Math.floor(Math.random() * 5)
+    console.log(randomNumber)
+});
+
+
+//give random number to picture2 
+$("#two").on("click", function(){
+    usersChoice = "two";
+});
+
+var randomNumber = Math.floor(Math.random() * 10)
+$("#two").on("click", function() {
+    var randomNumber = Math.floor(Math.random() * 10)
+    console.log(randomNumber)
+});
+
+
+//give random number to picture3
+$("#three").on("click", function(){
+    usersChoice = "three";
+});
+
+var randomNumber = Math.floor(Math.random() * 15)
+$("#three").on("click", function() {
+    var randomNumber = Math.floor(Math.random() * 15)
+    console.log(randomNumber)
+});
+
+
+//give random number to picture4
+$("#four").on("click", function(){
+    usersChoice = "four";
+});
+
+var randomNumber = Math.floor(Math.random() * 20)
+$("#four").on("click", function() {
+    var randomNumber = Math.floor(Math.random() * 20)
+    console.log(randomNumber)
+});
+
+
+//give random num to start
+$("#num").on("click", function() {
+var randomNumber = "";
+});
+
+
+$("#num").on("click", function() {
+    var randomNumber = Math.floor(Math.random() * 50)
+    console.log(randomNumber)
+});
+
+//add up score when you click on crystal
+// var userChoice = "";
+// var userScore = 0;
+// function win(userChoice, computerChoice) {
+//     userScore++;
+//     document.getElementById("pictures").innerHTML = userScore + " :";
+//     pictures.innerhtml = convertToWord(userChoice) + " beats " + convertToWord(computerChoice)  + "You Win.";
+// };
+// myFunction();
+
+// //if wins, else lose
+// $("#res").Math("click", function() {
+
+// }
+
+
+    // var w, x, y, z;
+    // w = Math.floor(Math.random());
+    // x = Math.floor(Math.random());
+    // y = Math.floor(Math.random());
+    // z = Math.floor(Math.random());
+    // document.getElementById("num").innerHTML = w + x + y + z; 
+
+    // $("num").html("click", function() {
+    //     var randomNumber = "";
+    // });
+
+    // $(document.getElementById("num").text) = Math.floor(Math.random());
+    // console.log(num)
